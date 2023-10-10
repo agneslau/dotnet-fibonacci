9bc1fbea0289706ddee61d9a7312f49606db0bac
Agnes <agnesgay@gmail.com>
2023-10-10 13:24:53 +0200

    commit

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -7,7 +7,6 @@ app.MapGet("/", () => "Hello World! 3");
 
 app.MapGet("/Fibonacci/{number}", (int number) => Fibonacci.Run(number).ToString());
 
-
 app.MapGet("/Fibonacci", () => Fibonacci.RunAsync(new []{"44", "44", "44"}));
 
 app.Run();
