2203261515a7529644288a0a162a74dd552e659c
Agnes <agnesgay@gmail.com>
2023-10-24 15:25:21 +0200

    adding db

--- src/Leonardo/Fibonacci.cs
+++ src/Leonardo/Fibonacci.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Leonardo;
 
 public class Fibonacci
@@ -8,6 +10,14 @@ public class Fibonacci
     }
     public static async Task<IList<int>> RunAsync(string[] args)
     {
+        await using var context = new FibonacciDataContext();
+        if (args.Length >= 100)
+        {
+            throw new ArgumentException("Too many arguments");
+        }
+        
+        
+        
         IList<int> results = new List<int>();
         var tasks = new List<Task<int>>();
         
@@ -16,25 +26,45 @@ public class Fibonacci
         
         foreach (var arg in args)
         {
-            var task = Task.Run(() =>
-            {
-                var result = Fibonacci.Run(int.Parse(arg));
-                //Console.WriteLine(result);
-                //Console.WriteLine($"Elapsed time : {stopwatch.ElapsedMilliseconds} ms");
-                return result;
-            });
-            tasks.Add(task);
-            //stopwatch.Stop();
-            //Console.WriteLine($"Total time : {stopwatch.ElapsedMilliseconds} ms");
-    
+                var tFibonnacci = await context.TFibonaccis
+                    .Where(t => t.FibInput == int.Parse(arg))
+                    .FirstOrDefaultAsync();
+
+                if (tFibonnacci == null)
+                {
+                    var task = Task.Run(() =>
+                    {
+                        var result = Fibonacci.Run(int.Parse(arg));
+                        //Console.WriteLine(result);
+                        //Console.WriteLine($"Elapsed time : {stopwatch.ElapsedMilliseconds} ms");
+
+                        return result;
+                        
+                        
+                    });
+                    
+                    tasks.Add(task);
+                }
+                else
+                {
+                    tasks.Add(Task.FromResult((int)tFibonnacci.FibOutput));
+                }
+
         }
         foreach (var task in tasks)
         {
             var result = await task;
+            context.TFibonaccis.Add(new TFibonacci()
+            {
+                FibOutput = result,
+                FibInput = int.Parse(args[tasks.IndexOf(task)]),
+            });
             
             Console.WriteLine($"Result: {task.Result}");
             results.Add(result);
         }
+
+        await context.SaveChangesAsync();
         return results;
     }
     
